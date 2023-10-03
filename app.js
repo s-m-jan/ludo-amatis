1f9ddcf9b31cdc2cd3f3da373f58c32b1f1b5823
Mehdi <mehdi.janfeshan2014@gmail.com>
2023-10-03 20:20:18 +0330

    center part ui completed

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-let projectArray = [
-    [0],
-    [[0],
-     [
-      [0], 
-      [
-        [0] ,
-        [
-            [[[0,0],[0,0]] ],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [[[0,0],[0,0]]],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [[[0,0],[0,0]]],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-            [[[0,0],[0,0]]]
-        ]
-      ]
-     ]
-    ],
-    [0]
-];
+let canvas = document.querySelector("canvas")
+
+
+let c = canvas.getContext("2d")
+
+c.beginPath();
+c.lineTo(0,0);
+c.lineTo( .5 * canvas.width, .5 *  canvas.height);
+c.lineTo(0, canvas.height);
+c.fillStyle = "blue"
+c.fill()
+c.closePath()
+
+c.beginPath();
+c.lineTo(0,0);
+c.lineTo(  canvas.width, 0);
+c.lineTo( .5 * canvas.width, .5 *  canvas.height);
+c.fillStyle = "red"
+c.fill()
+c.closePath()
+
+c.beginPath();
+c.lineTo(  canvas.width, 0);
+c.lineTo( .5 * canvas.width, .5 *  canvas.height);
+c.lineTo(canvas.width, canvas.height)
+c.fillStyle = "yellow"
+c.fill()
+c.closePath()
+
+c.beginPath();
+c.lineTo(  0, canvas.height);
+c.lineTo( .5 * canvas.width, .5 *  canvas.height);
+c.lineTo(canvas.width, canvas.height)
+c.fillStyle = "green"
+c.fill()
+c.closePath()
+
+
