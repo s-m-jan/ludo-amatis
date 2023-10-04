43439e4ea764158ddaa843cf59f1e573b9b3c811
Mehdi <mehdi.janfeshan2014@gmail.com>
2023-10-04 21:59:35 +0330

    dice almost finished

--- app.js
+++ app.js
@@ -39,14 +39,34 @@ let diceButton = document.querySelector("#diceButton");
 diceButton.addEventListener("click", changeDice);
 function changeDice(){
   let diceRandomNumber = Math.floor((Math.random()* 6) + 1)
-  console.log(diceRandomNumber)
+  
+  let imgWrapper = document.querySelector(".showDice");
+  let diceImage = document.querySelector(".showDice img");
+  imgWrapper.innerHTML = ""
+  let firstImg = document.createElement("img");
 
- if(diceRandomNumber == "1"){
-  document.querySelector(".showDice").innerHTML = <p>hello</p>
- }
- else if(diceRandomNumber == "2"){
-  document.querySelector(".showDice").innerHTML = <p>how are you</p>
- }
+  switch (diceRandomNumber) {
+    case 1:
+      firstImg.src = "./img/01.png";
+      break;
+    case 2:
+      firstImg.src = "./img/02.png";
+      break;
+    case 3:
+      firstImg.src = "./img/03.png";
+      break;
+    case 4:
+      firstImg.src = "./img/04.png";
+      break;
+    case 5:
+      firstImg.src = "./img/05.png";
+      break;
+    default:
+      firstImg.src = "./img/06.png";
+      break;
+  }
+
+
+
+  imgWrapper.appendChild(firstImg)
 }
-
-
